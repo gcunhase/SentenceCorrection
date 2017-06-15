7e9e6eb1e6f1340be655e55e35f53d711aa335c9
Gwena <gcunhase@users.noreply.github.com>
2017-06-15 16:33:51 +0900

    Create scores.sh

--- mtgru_files/scores.sh
+++ mtgru_files/scores.sh
@@ -24,7 +24,7 @@ TEST_FILE_EXP_1="newstest2013_small2.fr"
 TEST_FILE_OUT_1="newstest2013_small2_out_mtgru.txt"
 SCORES_FILE_1="newstest2013_small2_out_mtgru_scores.txt"
 
-python $PYTHON_SCRIPT --root_dir=$ROOT_DIR --test_file_in=$TEST_FILE_IN_1 --test_file_out=$TEST_FILE_OUT_1 --auto_decode=true --test_dir=$TEST_DIR
+#python $PYTHON_SCRIPT --root_dir=$ROOT_DIR --test_file_in=$TEST_FILE_IN_1 --test_file_out=$TEST_FILE_OUT_1 --auto_decode=true --test_dir=$TEST_DIR
 
 # Generate 1000 sentences test file and wait
 echo "Generate 1000 sentences test file"
@@ -33,28 +33,30 @@ TEST_FILE_EXP_2="newstest2013_1000.fr"
 TEST_FILE_OUT_2="newstest2013_1000_out_mtgru.txt"
 SCORES_FILE_2="newstest2013_1000_out_mtgru_scores.txt"
 
-python $PYTHON_SCRIPT --root_dir=$ROOT_DIR --test_file_in=$TEST_FILE_IN_2 --test_file_out=$TEST_FILE_OUT_2 --auto_decode=true --test_dir=$TEST_DIR
+#python $PYTHON_SCRIPT --root_dir=$ROOT_DIR --test_file_in=$TEST_FILE_IN_2 --test_file_out=$TEST_FILE_OUT_2 --auto_decode=true --test_dir=$TEST_DIR
 
 
 # Scores path
-SCORES_PYTHON_PATH="./metrics\ for\ translation\ performance/nlp-metrics-master/"
+SCORES_PYTHON_PATH="./metrics for translation performance/nlp-metrics-master/"
+SCORES_TEST_DIR="$SCORES_PYTHON_PATH""test-gwena/"
 
 # Copy files to scores directory
 echo "Copy files"
-cp $ROOT_DIR$TEST_DIR$TEST_FILE_OUT_1 $SCORES_PYTHON_PATH"test-gwena"
-cp $ROOT_DIR$TEST_DIR$TEST_FILE_OUT_2 $SCORES_PYTHON_PATH"test-gwena"
+cp $ROOT_DIR$TEST_DIR$TEST_FILE_OUT_1 "$SCORES_TEST_DIR"
+cp $ROOT_DIR$TEST_DIR$TEST_FILE_OUT_2 "$SCORES_TEST_DIR"
 
 
 # Run python file to get BLEU and ROUGE scores
 echo "Get scores"
-HYP_FILE=$TEST_FILE_OUT_1
-REF_FILE=$TEST_FILE_EXP_1
-python $SCORES_PYTHON_PATH"tester_allSentencesOneFile.py" --hyp_file=$HYP_FILE --ref_file=$REF_FILE --scores_file=$SCORES_FILE_1
+HYP_FILE="$SCORES_TEST_DIR"$TEST_FILE_OUT_1
+REF_FILE="$SCORES_TEST_DIR"$TEST_FILE_EXP_1
+SCORES_FILE_1="$SCORES_TEST_DIR"$SCORES_FILE_1
+python "$SCORES_PYTHON_PATH""tester_allSentencesOneFile.py" --hyp_file="$HYP_FILE" --ref_file="$REF_FILE" --scores_file="$SCORES_FILE_1"
 
-HYP_FILE=$TEST_FILE_OUT_2
-REF_FILE=$TEST_FILE_EXP_2
-python $SCORES_PYTHON_PATH"tester_allSentencesOneFile.py" --hyp_file=$HYP_FILE --ref_file=$REF_FILE --scores_file=$SCORES_FILE_2
+HYP_FILE="$SCORES_TEST_DIR"$TEST_FILE_OUT_2
+REF_FILE="$SCORES_TEST_DIR"$TEST_FILE_EXP_2
+SCORES_FILE_2="$SCORES_TEST_DIR"$SCORES_FILE_2
+python "$SCORES_PYTHON_PATH""tester_allSentencesOneFile.py" --hyp_file="$HYP_FILE" --ref_file="$REF_FILE" --scores_file="$SCORES_FILE_2"
 
 
 exit 0
-
