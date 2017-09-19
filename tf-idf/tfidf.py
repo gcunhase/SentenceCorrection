3f44650a8793eb9c7aa78ca6db14bf29eb0314a7
Gwena <gcunhase@users.noreply.github.com>
2017-09-19 22:01:39 +0900

    Update tfidf.py

--- tf-idf/tfidf.py
+++ tf-idf/tfidf.py
@@ -23,6 +23,7 @@ import operator
 import string, copy
 import pickle
 import re
+from math import log10
 
 class TfIdf:
     def __init__(self):
@@ -32,29 +33,35 @@ class TfIdf:
         self.total_number_words = 0
 
 
-    def add_docs_from_file(self, filename):
+    def add_docs_from_file(self, data_dir, dataset_dir, filename, new_data_dir):
         '''
-            TODO0: send all sentences as one
+            DONE: send all sentences as one
         '''	
-        text_file = open(filename,"r") 
+        text_file = open(data_dir+dataset_dir+filename,"r") 
         
-        line_count = 1
+        all_sents = ""
         for sentence in text_file:
             sentence = sentence.split("\n")[0]
-            self.add_doc_sentence(filename+"sent"+str(line_count), sentence)
-            line_count = line_count+1
-            
+	    all_sents = all_sents+sentence+" "
+
+        self.add_doc_sentence(data_dir, dataset_dir, filename, all_sents, new_data_dir)            
         text_file.close()
+
     
-    def add_doc_sentence(self, doc_name, sentence):
+    def add_doc_sentence(self, data_dir, dataset_dir, doc_name, sentence, new_data_dir):
         # Assume no special characters or markers
         # Old way of getting words
 	#list_of_words = sentence.split(" ")
 	# New way of getting words: https://stackoverflow.com/questions/6181763/converting-a-string-to-a-list-of-words
 	list_of_words = re.sub("[^\w]", " ",  sentence).split()
-        self.add_document(doc_name, list_of_words)
+        self.add_document(data_dir, dataset_dir, doc_name, list_of_words, new_data_dir)
 
-    def add_document(self, doc_name, list_of_words):
+    def add_document(self, data_dir, dataset_dir, doc_name, list_of_words, new_data_dir):
+        '''
+          Already being done: DF - most important words in corpus
+          DONE: documents.append - Implement TF x IDF for a document (and rank according to that value to get the most important words in file)
+        '''
+   
         #Lower or Upper case doesn't matter
         #Remove all special punctuation from word: https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
         table = string.maketrans("","")
@@ -74,15 +81,15 @@ class TfIdf:
             
         
         # normalizing the dictionary
-        length = float(len(list_of_words))
-        for k in doc_dict:
-            doc_dict[k] = doc_dict[k] / length
+        #doc_dict_norm = {}
+        #length = float(len(list_of_words))
+        #for k in doc_dict:
+        #    doc_dict_norm[k] = doc_dict[k] / length
 
-
+        
         # add the normalized document to the corpus
-        self.documents.append([doc_name, doc_dict])
-        
-        
+        self.documents.append([data_dir, new_data_dir, dataset_dir, doc_name, len(list_of_words), doc_dict])
+                
     def get_dictionary(self):
         #print("\n")
         #print(list(self.corpus_dict.items()))
@@ -113,6 +120,7 @@ class TfIdf:
         pickle.dump(self.corpus_dict, output)
         sorted_x = sorted(self.corpus_dict.items(), key=operator.itemgetter(1))
         pickle.dump(sorted_x, output)
+        pickle.dump(self.documents, output)
         output.close()
 
     def load_dictionary_from_pickle(self, filename):
@@ -120,30 +128,34 @@ class TfIdf:
         total_number_words = pickle.load(output)
         corpus_dict = pickle.load(output)
         sorted_x = pickle.load(output)
+        documents = pickle.load(output)
         output.close()
-        return total_number_words, corpus_dict, sorted_x
+        return total_number_words, corpus_dict, sorted_x, documents
 
     def save_dictionary_in_file(self, filename):
+        
+        # Save entire corpus in file
+        print("Corpus...")
         words = "# words: "+str(self.total_number_words)
-        print(words)
+        #print(words)
         dist_words = "# distinct words: "+str(len(self.corpus_dict))
-        print(dist_words)
-        #print("# mean frequency")
-        #print("# standard deviation")
-        #Sort dictionary from most to less frequent occurrences
-        # It is not possible to sort a dict, only to get a representation of a
-        #  dict that is sorted. Dicts are inherently orderless, but other types,
-        #  such as lists and tuples, are not. So you need a sorted representation,
-        #  which will be a list-probably a list of tuples
+        #print(dist_words)
+        ##print("# mean frequency")
+        ##print("# standard deviation")
+        ##Sort dictionary from most to less frequent occurrences
+        ## It is not possible to sort a dict, only to get a representation of a
+        ##  dict that is sorted. Dicts are inherently orderless, but other types,
+        ##  such as lists and tuples, are not. So you need a sorted representation,
+        ##  which will be a list-probably a list of tuples
         sorted_x = sorted(self.corpus_dict.items(), key=operator.itemgetter(1))
-        header = "Rank Word Occurrence Normalized Occurrence"
-        print(header)
+        header = "Rank - Word - Occurrence - Normalized Occurrence"
+        #print(header)
         info_str = ""
         for i in range(0,len(sorted_x)):
             corpus_item = sorted_x[i]
             info = str(i+1)+" "+corpus_item[0]+" "+str(corpus_item[1])+" "+str(round(corpus_item[1]/self.total_number_words, 4))
             info_str = info_str+info+"\n"
-            print(info)
+            #print(info)
             
         file_str = words+"\n"+dist_words+"\n\n"+header+"\n"+info_str+"\n"
         f = open(filename+".txt", 'w')
@@ -157,21 +169,66 @@ class TfIdf:
             corpus_item = sorted_inv_x[i]
             info = str(i+1)+" "+corpus_item[0]+" "+str(corpus_item[1])+" "+str(round(corpus_item[1]/self.total_number_words, 4))
             info_str = info_str+info+"\n"
-            print(info)
+            #print(info)
         file_inv_str = words+"\n"+dist_words+"\n\n"+header+"\n"+info_str+"\n"
         f_inv = open(filename+"_inv.txt", 'w')        
         f_inv.write(file_inv_str)
         f_inv.close()
+        
+        
+        # Save multiple files' dictionary -> tfidf
+        print("Documents...")
+        #print(self.documents[0][4].items()[0][0])
+        N = len(self.documents)
+        for i in range(0, N):
+            data_dir = self.documents[i][0]
+            new_data_dir = self.documents[i][1]
+            dataset_dir = self.documents[i][2]
+            doc_name = self.documents[i][3]
+            print("\n"+dataset_dir+doc_name)
+            words_in_doc = self.documents[i][4]            
+            doc_dict = self.documents[i][5]
+            
+            words = "# words: "+str(words_in_doc)
+            dist_words = "# distinct words: "+str(len(doc_dict))
+            
+            tfidf = {}
+            for j in range(0, len(doc_dict)):
+                item = doc_dict.items()[j]
+                w = item[0]
+                print("word: "+w)
+                tf = float(item[1]) / float(words_in_doc)
+                print("tf: "+str(tf))
+                df = float(self.corpus_dict.get(w))
+                print("df: "+str(df))
+                tfidf[w] = tf * log10(N / df)
+                print("tfidf: "+str(tfidf.get(w)))
+                
+            sorted_doc_inv_x = sorted(tfidf.items(), key=operator.itemgetter(1), reverse=True)
+            #sorted_inv_x.reverse()
+            #print(sorted_x)
+        
+            header = "Rank - Word - tfidf"
+            info_str = ""
+            for j in range(0,len(sorted_doc_inv_x)):
+                doc_item = sorted_doc_inv_x[j]
+                info = str(j+1)+" "+doc_item[0]+" "+str(doc_item[1])
+                info_str = info_str+info+"\n"
+                print(info)
+                   
+           
+            # save tfidf dictionary in respective file
+            if not os.path.exists(new_data_dir+dataset_dir):
+                os.makedirs(new_data_dir+dataset_dir)
+            #write new_data_dir+dataset_dir
+            file_str = words+"\n"+dist_words+"\n\n"+header+"\n"+info_str+"\n"
+            f_doc = open(new_data_dir+dataset_dir+doc_name.split(".")[0]+".tfidf", 'w')        
+            f_doc.write(file_str)
+            f_doc.close()
+        
+                
         return sorted_x
     
-    '''
-    TODO1: Implement TF x IDF for entire corpus (and rank according to that value to get the most important words in corpus)
-    '''
-
-    '''
-    TODO2: Implement TF x IDF for a document (and rank according to that value to get the most important words in file)
-    '''
-
     
     def similarities(self, list_of_words):
         """Returns a list of all the [docname, similarity_score] pairs relative to a
@@ -200,3 +257,4 @@ list of words.
             sims.append([doc[0], score])
 
         return sims
+
